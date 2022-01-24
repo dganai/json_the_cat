82e5436e47899d15c5e857bf8072a4a3731b724f
dganai <dinaganai@gmail.com>
2022-01-24 22:27:50 +0000

    refactor and eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,26 +4,27 @@ const request = require('request'); // request data from api breed search
 // print out the body content to the terminal
 // fetch siberian data from the api endpoint using request
 const fetchBreedDescription =  (breedName, callback) => {
-
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, res, body) => {
+    const [data] = JSON.parse(body);
     
+    // if URL isn't valid, output error msg
     if (err) {
       callback(err, null);
       return;
     }
-    const data = JSON.parse(body);
-
-    if (!data[0]) {
-      callback('Breed does not exist. Try again.', null);
-      return;
-    }
+   
+    // if no error but no valid breed found
+    if (!err) {
+      if (data) {
+        return callback(null, data.description);
+      }
       
-      callback(null, data[0].description);
+      callback('Breed not found.', null);
       
     
     }
 
-  )
+  });
 
 };
 
--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 const breedName = process.argv[2];
 
-fetchBreedDescription(breedName, (error, description) => {
-if (error) {
-  console.log('Error fetch details:', error);
-} else {
-  console.log(description)
-}
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
 });
 
